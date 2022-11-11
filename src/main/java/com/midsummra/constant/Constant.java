95e70b73b03fd3ef686aa99cd4edcee7a2d54695
Midsummra <midsummra@gmail.com>
2022-11-11 17:09:03 +0800

    2022.11.11-2 添加了webConfig.properties用于配置参数

---
+++ src/main/java/com/midsummra/constant/Constant.java
@@ -0,0 +1,25 @@
+package com.midsummra.constant;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Properties;
+
+public class Constant {
+    private static Properties properties = null;
+    static {
+        try {
+            String path = Constant.class.getClassLoader().getResource("/com/midsummra/constant/webConfig.properties").getPath();
+            FileInputStream fis = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final int LikeThresholds = Integer.parseInt(properties.getProperty("LikeThresholds"));
+    public static final int TransformThresholds = Integer.parseInt(properties.getProperty("TransformThresholds"));
+    public static final int QuestionnaireLimits = Integer.parseInt(properties.getProperty("QuestionnaireLimits"));
+}
