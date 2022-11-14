3fd679fab4d6cfd3240b816be71481bdfd8396b0
Midsummra <midsummra@gmail.com>
2022-11-14 22:30:55 +0800

    2022.11.14-2 C3P0 -> HikariCP

--- src/main/java/com/midsummra/controller/SessionController.java
+++ src/main/java/com/midsummra/controller/SessionController.java
@@ -5,6 +5,7 @@ import com.mysql.cj.util.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,17 +19,18 @@ public class SessionController {
 
     @RequestMapping(value = "/isLiked",produces = "text/html;charset = utf-8")
     @ResponseBody
-    public String getTranslationSession(String[] translations, HttpServletRequest request) throws Exception{
+    public String getTranslationSession(@RequestParam(value = "translations") String[] translations, HttpServletRequest request) throws Exception{
 
         HashMap<String, String> map = new HashMap<>();
         HttpSession session = request.getSession();
 
-        for (String translation : translations) {
-            if (session.getAttribute(translation).equals("0") || ObjectUtils.isEmpty(session.getAttribute(translation))){
-                map.put(translation,"0");
+        for (int i = 0; i < translations.length; i++) {
+            if (ObjectUtils.isEmpty(session.getAttribute(translations[i]))|| ((String)session.getAttribute(translations[i])).equals("0")){
+                map.put(translations[i],"0");
             }else {
-                map.put(translation,"1");
+                map.put(translations[i],"1");
             }
+
         }
 
         ObjectMapper mapper = new ObjectMapper();
