b34e10e10c6009118dbc6ceb1ecef4f0134d6416
IkarashiAkatuki <109781981+IgarashiAkatuki@users.noreply.github.com>
2022-12-01 19:50:25 +0800

    Delete src directory

--- src/main/java/com/midsummra/service/ErratumService.java
+++
@@ -1,9 +0,0 @@
-package com.midsummra.service;
-
-import com.midsummra.pojo.Erratum;
-
-public interface ErratumService {
-    public int addErratum(Erratum erratum);
-    public int deleteErratum(int id);
-    public Erratum queryAllErratum();
-}
